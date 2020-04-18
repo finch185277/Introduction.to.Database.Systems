4c64be4edc7ec0682169321babddf1faadf24ef5
finch185277 <59487andy@gmail.com>
2020-04-18 21:13:42 +0800

    Add 8.sql

---
+++ hw1/8.sql
@@ -0,0 +1,7 @@
+select c.champion_name
+from champ c
+where c.champion_id not in (
+  select distinct t.champion_id
+  from match_info m, teamban t
+  where m.match_id = t.match_id and SUBSTRING_INDEX(m.version, '.', 2) like '7.7'
+);
