85d540c82fc21f5eecbdc9e74b01004e9f5aac26
finch185277 <59487andy@gmail.com>
2020-04-19 14:30:15 +0800

    Add 12.sql

---
+++ hw1/12.sql
@@ -0,0 +1,23 @@
+(
+  select 'Flash on [D]' as Flash_Position, dw.win as win, dl.lose as lose, dw.win / dl.lose as win_ratio, dw.gold / dw.win as win_avg_gold, dl.gold / dl.lose as lose_avg_gold
+  from (
+    select count(*) as win, sum(s.goldearned) as gold
+    from participant p, stat s
+    where p.player_id = s.player_id and p.ss1 like 'Flash%' and s.win = 1
+  ) as dw, (
+    select count(*) as lose, sum(s.goldearned) as gold
+    from participant p, stat s
+    where p.player_id = s.player_id and p.ss1 like 'Flash%' and s.win = 0
+  ) as dl
+) union all (
+  select 'Flash on [F]' as Flash_Position, dw.win as win, dl.lose as lose, dw.win / dl.lose as win_ratio, dw.gold / dw.win as win_avg_gold, dl.gold / dl.lose as lose_avg_gold
+  from (
+    select count(*) as win, sum(s.goldearned) as gold
+    from participant p, stat s
+    where p.player_id = s.player_id and p.ss2 like 'Flash%' and s.win = 1
+  ) as dw, (
+    select count(*) as lose, sum(s.goldearned) as gold
+    from participant p, stat s
+    where p.player_id = s.player_id and p.ss2 like 'Flash%' and s.win = 0
+  ) as dl
+);
