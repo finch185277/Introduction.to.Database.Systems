571afb7a3747a6c683008b9cfcf4d30858d5ed2d
finch185277 <59487andy@gmail.com>
2020-04-19 22:19:54 +0800

    Update 9.sql

--- hw1/9.sql
+++ hw1/9.sql
@@ -4,28 +4,40 @@ from (
   from
   (
     (
-      select SUBSTRING_INDEX(wm.version, '.', 2) as version, convert(count(wm.match_id), unsigned) as win_cnt, convert(0, unsigned) as lose_cnt
+      select SUBSTRING_INDEX(wm.version, '.', 2) as version,
+        convert(count(wm.match_id), unsigned) as win_cnt, convert(0, unsigned) as lose_cnt
       from (
         select wps1.match_id, wps1.win
         from (
-          select wp1.match_id, ws1.win, wp1.champion_id from participant wp1, stat ws1 where wp1.player_id = ws1.player_id
+          select wp1.match_id, ws1.win, wp1.champion_id
+          from participant wp1, stat ws1
+          where wp1.player_id = ws1.player_id
         ) as wps1, (
-          select wp2.match_id, ws2.win, wp2.champion_id from participant wp2, stat ws2 where wp2.player_id = ws2.player_id
+          select wp2.match_id, ws2.win, wp2.champion_id
+          from participant wp2, stat ws2
+          where wp2.player_id = ws2.player_id
         ) as wps2
-        where wps1.match_id = wps2.match_id and wps1.win = wps2.win and wps1.champion_id = 17 and wps2.champion_id = 64
+        where wps1.match_id = wps2.match_id and wps1.win = wps2.win
+          and wps1.champion_id = 17 and wps2.champion_id = 64
       ) as wps, match_info wm
       where wm.match_id = wps.match_id and wps.win = 1
       group by SUBSTRING_INDEX(wm.version, '.', 2)
     ) union all (
-      select SUBSTRING_INDEX(lm.version, '.', 2) as version, convert(0, unsigned) as win_cnt, convert(count(lm.match_id), unsigned) as lose_cnt
+      select SUBSTRING_INDEX(lm.version, '.', 2) as version,
+        convert(0, unsigned) as win_cnt, convert(count(lm.match_id), unsigned) as lose_cnt
       from (
         select lps1.match_id, lps1.win
         from (
-          select lp1.match_id, ls1.win, lp1.champion_id from participant lp1, stat ls1 where lp1.player_id = ls1.player_id
+          select lp1.match_id, ls1.win, lp1.champion_id
+          from participant lp1, stat ls1
+          where lp1.player_id = ls1.player_id
         ) as lps1, (
-          select lp2.match_id, ls2.win, lp2.champion_id from participant lp2, stat ls2 where lp2.player_id = ls2.player_id
+          select lp2.match_id, ls2.win, lp2.champion_id
+          from participant lp2, stat ls2
+          where lp2.player_id = ls2.player_id
         ) as lps2
-        where lps1.match_id = lps2.match_id and lps1.win = lps2.win and lps1.champion_id = 17 and lps2.champion_id = 64
+        where lps1.match_id = lps2.match_id and lps1.win = lps2.win
+          and lps1.champion_id = 17 and lps2.champion_id = 64
       ) as lps, match_info lm
       where lm.match_id = lps.match_id and lps.win = 0
       group by SUBSTRING_INDEX(lm.version, '.', 2)
