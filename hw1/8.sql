ab4e35543a67f34547704152ca0de3015eff6505
finch185277 <59487andy@gmail.com>
2020-04-20 11:28:04 +0800

    Update 8.sql

--- hw1/8.sql
+++ hw1/8.sql
@@ -4,4 +4,5 @@ where c.champion_id not in (
   select distinct t.champion_id
   from match_info m, teamban t
   where m.match_id = t.match_id and SUBSTRING_INDEX(m.version, '.', 2) like '7.7'
-);
+)
+order by c.champion_name;
