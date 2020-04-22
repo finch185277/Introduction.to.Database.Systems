ee7e0ec042f498f1f929b6dae9c8b20d372bc27a
finch185277 <59487andy@gmail.com>
2020-04-22 22:08:24 +0800

    Update create_tables.sql

--- hw1/create_tables.sql
+++ hw1/create_tables.sql
@@ -58,33 +58,33 @@ load data local infile './champs.csv'
 into table champ
 fields terminated by ','
 enclosed by '"'
-lines terminated by '\n'
+lines terminated by '\r\n'
 ignore 1 lines;
 
 load data local infile './matches.csv'
 into table match_info
 fields terminated by ','
 enclosed by '"'
-lines terminated by '\n'
+lines terminated by '\r\n'
 ignore 1 lines;
 
 load data local infile './participants.csv'
 into table participant
 fields terminated by ','
 enclosed by '"'
-lines terminated by '\n'
+lines terminated by '\r\n'
 ignore 1 lines;
 
 load data local infile './teambans.csv'
 into table teamban
 fields terminated by ','
 enclosed by '"'
-lines terminated by '\n'
+lines terminated by '\r\n'
 ignore 1 lines;
 
 load data local infile './stats.csv'
 into table stat
 fields terminated by ','
 enclosed by '"'
-lines terminated by '\n'
+lines terminated by '\r\n'
 ignore 1 lines;
