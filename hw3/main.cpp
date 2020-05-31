1ec3f37ba70b5ebd90222dc3fe381b502a9c760d
finch185277 <59487andy@gmail.com>
2020-05-31 10:59:04 +0800

    Update hw3

---
+++ hw3/main.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sstream>
+#include <vector>
+
+int main(int argc, char **argv) {
+  int thread_nums = atoi(argv[1]);
+  std::string outfile = argv[2];
+
+  return 0;
+}
